acc2ffa874c2095e7fc0fbfb9fe1b0ec00535df5
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2023-05-09 19:20:33 -0500

    improved shader - ensured shader ordering, n of shader loads allowed

--- GameApp/shader.cpp
+++ GameApp/shader.cpp
@@ -1,83 +1,155 @@
 #include "shader.hpp"
 
-Shader::Shader(const char* vertexPath, const char* fragmentPath)
+bool IsValidShaderType(int type) {
+	switch (type)
+	{
+	case GL_VERTEX_SHADER:
+	case GL_FRAGMENT_SHADER:
+	case GL_TESS_CONTROL_SHADER:
+	case GL_TESS_EVALUATION_SHADER:
+		return true;
+
+	default: return false;
+	}
+}
+
+int GetShaderOrder(int type) {
+
+	switch (type)
+	{
+	case GL_VERTEX_SHADER: return 0;
+	case GL_FRAGMENT_SHADER: return 1;
+	case GL_TESS_CONTROL_SHADER: return 2;
+	case GL_TESS_EVALUATION_SHADER:return 3;
+	default:
+		string msg("GetShaderOrder - Provided an invalid type.");
+		cerr << msg << " - " << type << endl;
+		throw std::invalid_argument(msg);
+	}
+}
+
+bool ShaderOrdering(const ShaderFileInfo& a, const ShaderFileInfo& b) {
+	return GetShaderOrder(a.Type) < GetShaderOrder(b.Type);
+}
+
+ShaderFileInfo::ShaderFileInfo(const string_view& path, optional<int> type)
 {
-	// 1. retrieve the vertex/fragment source code from filePath
-	std::string vertexCode;
-	std::string fragmentCode;
-	std::ifstream vShaderFile;
-	std::ifstream fShaderFile;
-	// ensure ifstream objects can throw exceptions:
-	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	try
+	if (!std::filesystem::exists(path))
 	{
-		// open files
-		vShaderFile.open(vertexPath);
-		fShaderFile.open(fragmentPath);
-		std::stringstream vShaderStream, fShaderStream;
-		// read file's buffer contents into streams
-		vShaderStream << vShaderFile.rdbuf();
-		fShaderStream << fShaderFile.rdbuf();
-		// close file handlers
-		vShaderFile.close();
-		fShaderFile.close();
-		// convert stream into string
-		vertexCode = vShaderStream.str();
-		fragmentCode = fShaderStream.str();
-	}
-	catch (std::ifstream::failure e)
-	{
-		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-	}
-	const char* vShaderCode = vertexCode.c_str();
-	const char* fShaderCode = fragmentCode.c_str();
-
-	// 2. compile shaders
-	uint vertex, fragment;
-	int success;
-	char infoLog[512];
-
-	// vertex Shader
-	vertex = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex, 1, &vShaderCode, NULL);
-	glCompileShader(vertex);
-	// print compile errors if any
-	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	};
-
-	// similiar for Fragment Shader
-	fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment, 1, &fShaderCode, NULL);
-	glCompileShader(fragment);
-	// print compile errors if any
-	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	};
-
-	// shader Program
-	Id = glCreateProgram();
-	glAttachShader(Id, vertex);
-	glAttachShader(Id, fragment);
-	glLinkProgram(Id);
-	// print linking errors if any
-	glGetProgramiv(Id, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(Id, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		string msg("ShaderFileInfo - ctor - Provided path doesn't exist.");
+		cerr << msg << " - " << path << endl;
+		throw std::invalid_argument(msg);
 	}
 
-	// delete the shaders as they're linked into our program now and no longer necessary
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
+	if (!type.has_value())
+	{
+		string upperPath(path);
+		transform(upperPath.begin(), upperPath.end(), upperPath.begin(), ::toupper);
+
+		if (upperPath.ends_with(".VERT.GLSL"))
+			type = GL_VERTEX_SHADER;
+		else if (upperPath.ends_with(".FRAG.GLSL"))
+			type = GL_FRAGMENT_SHADER;
+		else if (upperPath.ends_with(".TCONT.GLSL"))
+			type = GL_TESS_CONTROL_SHADER;
+		else if (upperPath.ends_with(".TEVAL.GLSL"))
+			type = GL_TESS_EVALUATION_SHADER;
+		else
+		{
+			string msg("ShaderFileInfo - ctor - Couldn't determine the type by path.");
+			cerr << msg << " - " << path << endl;
+			throw std::invalid_argument(msg);
+		}
+	}
+
+	if (!IsValidShaderType(type.value()))
+	{
+		string msg("ShaderFileInfo - ctor - Provided type isn't a valid shader type.");
+		cerr << msg << " - " << type.value() << endl;
+		throw std::invalid_argument(msg);
+	}
+
+	Path = path;
+	Type = type.value();
+}
+
+const string_view ShaderFileInfo::GetTypeName(optional<string_view> defaultName)
+{
+	switch (Type)
+	{
+	case GL_VERTEX_SHADER: return "VERTEX";
+	case GL_FRAGMENT_SHADER: return "FRAGMENT";
+	case GL_TESS_CONTROL_SHADER: return "TESS-CONT";
+	case GL_TESS_EVALUATION_SHADER: return "TESS-EVAL";
+	default: return defaultName.value_or("<UNKNOWN>");
+	}
+}
+
+Shader::Shader(vector<ShaderFileInfo> shaderFileInfos) : Id(0)
+{
+	std::sort(shaderFileInfos.begin(), shaderFileInfos.end(), ShaderOrdering);
+	for (ShaderFileInfo& shaderFileInfo : shaderFileInfos)
+	{
+		// 1. retrieve the vertex/fragment source code from filePath
+		string_view shaderTypeName = shaderFileInfo.GetTypeName();
+		string sourceCode;
+		std::ifstream fileStream;
+		// ensure ifstream objects can throw exceptions:
+		fileStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			// open files
+			fileStream.open(shaderFileInfo.Path.data());
+			std::stringstream stringStream;
+			// read file's buffer contents into streams
+			stringStream << fileStream.rdbuf();
+			// close file handlers
+			fileStream.close();
+			// convert stream into string
+			sourceCode = stringStream.str();
+		}
+		catch (std::ifstream::failure e)
+		{
+			std::cout << "ERROR::SHADER::" << shaderTypeName << "::FILE_NOT_SUCCESFULLY_READ - " << shaderFileInfo.Path << std::endl;
+		}
+		const char* sourceCodePtr = sourceCode.c_str();
+
+		// 2. compile shaders
+		const int infoLogLength = 1052;
+		uint shaderPtr;
+		int success;
+		char infoLog[infoLogLength];
+
+		// vertex Shader
+		shaderPtr = glCreateShader(shaderFileInfo.Type);
+		glShaderSource(shaderPtr, 1, &sourceCodePtr, NULL);
+		glCompileShader(shaderPtr);
+		// print compile errors if any
+		glGetShaderiv(shaderPtr, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shaderPtr, infoLogLength, NULL, infoLog);
+			std::cout << "ERROR::SHADER::" << shaderTypeName << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
+		// shader Program
+		if (Id == 0)
+		{
+			Id = glCreateProgram();
+		}
+		glAttachShader(Id, shaderPtr);
+		glLinkProgram(Id);
+		// print linking errors if any
+		glGetProgramiv(Id, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(Id, infoLogLength, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
+
+		// delete the shaders as they're linked into our program now and no longer necessary
+		glDeleteShader(shaderPtr);
+	}
 }
 
 void Shader::Use()
